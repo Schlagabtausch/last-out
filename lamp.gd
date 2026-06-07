bf1e098e623b5f1414783dd9a8d6194fc0b97ed7
Schlagabtausch <121886511+Schlagabtausch@users.noreply.github.com>
2026-06-07 22:24:52 +0200

    added lightTextures and lamps, and flickering scripts

---
+++ lamp.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var light: PointLight2D = $PointLight2D
+
+@export var base_energy: float = 0.8
+@export var flicker_amount: float = 0.08
+@export var flicker_speed: float = 8.0
+
+var time := 0.0
+
+
+func _process(delta: float) -> void:
+	time += delta * flicker_speed
+	var flicker := sin(time * 1.7) * flicker_amount + sin(time * 3.1) * flicker_amount * 0.5
+	light.energy = base_energy + flicker
